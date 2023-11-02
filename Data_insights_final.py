a704e52cac3e63ff0e91cd51cdcf49e47b660546
CRWill97 <91710270+CRWill97@users.noreply.github.com>
2023-11-02 19:33:48 -0400

    Update Data_insights_final.py
    Replaced langchain package with langchain_experimental.agents

--- Data_insights_final.py
+++ Data_insights_final.py
@@ -7,13 +7,13 @@
 #import streamlit as st
 import pandas as pd
 from langchain.chat_models import ChatOpenAI
-from langchain.agents import create_pandas_dataframe_agent
+from langchain_experimental.agents import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
 import tabulate
 import pyarrow
 import openai
 
-
+#langchain_experimental.agents.create_pandas_dataframe_agent
 # In[75]:
 
 
